d11d04f8e92a3387afc499a147e099dc8bcd0e14
reveluv1814 <neilgraneros11@gmail.com>
2022-09-28 15:37:24 -0400

    create task terminado

---
+++ server/controllers/task.controllers.js
@@ -0,0 +1,31 @@
+import { pool } from "../db.js";
+
+export const getTasks = (req, res) => {
+  res.send("obteniendo tareas");
+};
+
+export const getTask = (req, res) => {
+  res.send("obteniendo una tarea");
+};
+
+export const createTask = async (req, res) => {
+  console.log(req.body);
+  const { title, description } = req.body;
+  const [result] = await pool.query(
+    "INSERT INTO tasks(title,description) VALUES(?,?)",
+    [title, description]
+  );
+  res.json({
+    id: result.insertId,
+    title,
+    description,
+  });
+};
+
+export const updateTask = (req, res) => {
+  res.send("actualizando tarea");
+};
+
+export const deleteTask = (req, res) => {
+  res.send("eliminando tarea");
+};
